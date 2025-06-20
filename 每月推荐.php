895d39b84616374a9849d07fffa6a561496509d4
2439905184 <2439905184@qq.com>
2025-06-21 06:21:42 +0800

    更新

---
+++ 每月推荐.php
@@ -0,0 +1,13 @@
+<?php
+// 应该为动态内容
+function getRecommand(type)
+{
+    if(type == "software")
+    {
+        $recommand = {
+            "softA":"a.exe"
+        }
+        return Recommand
+    }
+}
+?>
